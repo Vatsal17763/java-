6acd74eb153c577174ba38ab8af24dac4204ddcf
Vatsal17763 <minds3813@gmail.com>
2026-02-03 13:22:02 +0530

    function_3-2-26

--- function.js
+++ function.js
@@ -10,26 +10,26 @@
 // type of functions:
 
 // function name(params) {}  --> function declaration
-function abc() {
+// function abc() {
 
-}
+// }
 
 // let fnc = function () {}  --> function expression
-let fnc = function () {
+// let fnc = function () {
 
-}
+// }
 
 // let fnc = () => {}  --> arrow function --> fat arrow function
-let fnc1 = () => {
+// let fnc1 = () => {
 
-}
+// }
 
-function temp_cart() {
-  console.log("Adding Producat");
-}
-temp_cart();
-temp_cart();
-temp_cart();
+// function temp_cart() {
+//   console.log("Adding Producat");
+// }
+// temp_cart();
+// temp_cart();
+// temp_cart();
 
 // parameters vs arguments
 // parameters are the names listed in the function definition
@@ -37,68 +37,271 @@ temp_cart();
 // arguments are the real values passed to the function (args -->
 // function call karti () ni andar lakheli(pass) kareli values).
 
-function cart(producat) {
+// function cart(producat) {
 // product parameters
-console.log("product", producat)
-}
-cart("Mobile"); // mobile arguments
-cart("Laptop");
-cart("Tablet");
+// console.log("product", producat)
+// }
+// cart("Mobile"); // mobile arguments
+// cart("Laptop");
+// cart("Tablet");
 
 // convert into function expression
 // let cart = function(){}
-let cart1 = function (producat) {
+// let cart1 = function (producat) {
 // product-params
-console.log("product", producat)
-}
-cart1("Mobile"); // mobile -args
-cart1("Laptop");
-cart1("Tablet");
+// console.log("product", producat)
+// }
+// cart1("Mobile"); // mobile -args
+// cart1("Laptop");
+// cart1("Tablet");
 
 
 // Default, rest and spread parameters in function
 // default
-function abc(v1, v2) {
-  console.log(v1, v2);
-}
-abc(20, 21);
+// function abc(v1, v2) {
+//   console.log(v1, v2);
+// }
+// abc(20, 21);
 
-function cart3(price, product = "product") {
-  console.log(product, price)
-}
-cart3(20000);
+// function cart3(price, product = "product") {
+//   console.log(product, price)
+// }
+// cart3(20000);
 
 // rest -> Jayre function ma multiple arguments pass karva to vadhare params banava
 // pade chhe mate te na karvu pade te mate rest parameter use kariye chhiye (rest ->
 // jo funtion na parameter ni anadar lakhvama aave chhe)
 
-function abcd(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) {
-  console.log(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10);
-}
-abcd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// function abcd(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) {
+//   console.log(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10);
+// }
+// abcd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-function abcd1(...numbers) {
-  console.log(numbers);
-}
-abcd1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); // how to handle multiple arguments
+// function abcd1(...numbers) {
+//   console.log(numbers);
+// }
+// abcd1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); // how to handle multiple arguments
 
-function abcd2(v1, v2, ...numbers) {
-  console.log(v1, v2, numbers);
-}
-abcd2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// function abcd2(v1, v2, ...numbers) {
+//   console.log(v1, v2, numbers);
+// }
+// abcd2(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 // retrun or early return
 // retrun --> function mathi value bahar mokalva mate
-function getValuel(value) {
-  if (value < 25) return "Value is less than 25";
-  else if (value < 50) return "Value is less than 50";
-  else if (value < 75) return "Value is less than 75";
-  return "Value is 100 or more";
-}
-let result = getValue1(80);
-console.log(result);
+// function getValuel(value) {
+//   if (value < 25) return "Value is less than 25";
+//   else if (value < 50) return "Value is less than 50";
+//   else if (value < 75) return "Value is less than 75";
+//   return "Value is 100 or more";
+// }
+// let result = getValue1(80);
+// console.log(result);
 
 // first Class Function
 // function can be treated as variables
 // function can be passed as arguments to other functions
 // function can be returned from other functions.
+
+// first Class Function:
+// function can be treated as variable
+// let fnc = function(){}
+// let fnc = () => {}
+
+// let buyNow1 = function (product, price){
+//     console.log(product, price)
+// }
+
+// buyNow1("S24 FE", 50500);
+
+// function can be passed as arguments to other functions
+
+// function can be returned from other function
+
+// function can be passed as arguments to other functions
+// let fnc = function(){}
+// fnc(function(){})
+
+// function abc1(v1, v2){
+//     console.log(v2);
+//     return v1();
+// }
+
+// abc1(function(){
+//     console.log("first class function")
+// }, "Hello")
+
+// function abc2(v1, v2){
+//     return v1();
+//     console.log(v2);
+// }
+
+// abc2(function(){
+//     console.log("first class function")
+// }, "Hello")
+
+
+// function can be returned from other function
+// function abc(){
+//     return function (){
+//         console.log("function within function")
+//     }
+// }
+
+// abc()();
+
+// function abc2(){
+//     return () => {
+//         console.log("arrow function within function")
+//     }
+// }
+
+// abc2();
+
+// higher order function (HOF)
+// function that takes another function as an argument or returns a
+// function as a result (ya function je return kare ek function athava
+// accept kare ek function params ma)
+
+// function abcd(val){ val(); }
+// -> abcd(function(){ console.log("hello") })
+// function abcd(val){} --> higher order function
+
+// function abcd(){ return function(){} } abcd()() --> higher order
+
+// pure vs impure function
+// pure function --> function je same input par same output aapse ane
+// koi bahar na state ne modify na kare (pure function --> je function
+// bahar na state ne modify na kare)
+
+// let a = 20;
+// function change_a(){
+//     return "a not change(pure function) " + a;
+// } // -- pure function
+
+// console.log(change_a());
+
+// impure function --> function je same input par different output
+// aapse athava bahar na state ne modify kare (impure function --> je
+// function bahar na state ne modify kare)
+// function change_a1(){
+//     a++;
+//     return "a change (impure function)" + a;
+// }
+// console.log(change_a1())
+
+// closure function
+// function je potana parent function na variables ne access kari
+// shake (return thava valo function use karshe parent function na
+// variable) (function within function)
+
+// function outer(){
+//     let outer_var = "Outer Function Variable";
+
+//     function inner(){
+//         console.log(outer_var);
+//     }
+
+//     inner();
+// }
+
+// outer();
+
+
+// lexical scope -> Nested function can access variables declared in their outer scope
+// function outer1() {
+//     let outer_var = "Outer Function Variable";
+
+//     function inner1() {
+//         console.log(outer_var);
+//         let inner_var = "Inner Function Variable";
+
+//         function most_inner() {
+//             console.log(outer_var);
+//             console.log(inner_var);
+//         }
+
+//         most_inner();
+//     }
+
+//     inner1();
+// }
+
+// outer1();
+
+
+// IIFE - Immediately Invoked Function Expression
+// (function () {})(); // function je declare karta j call thai jaye
+// (function () {
+//   console.log("IIFE executed");
+// })();
+
+
+// Hoisting in function
+// use case --> show project structure
+// temp_fnc();
+
+// function temp_fnc(){
+//   console.log("hoisting in function")
+// }
+
+
+//example
+//1
+// greet(); 
+
+// function greet(){
+//   console.log("Hello");
+// }
+//2
+// greet();
+// function greet(){
+//   console.log("Hello!");
+// }
+//3
+// const add = (a, b) => a + b;
+//4
+// function welcome(name){ 
+//   console.log("Welcome " + name);
+// }
+
+// welcome("user");          
+//5
+// function temp (a, b, c){  
+//   console.log(a,b,c);
+// }
+
+// temp(1, 2) // 2 arguments
+//6
+// function temp_user(name = "Guest"){
+//   console.log("Hello " + name);
+// }
+
+// temp_user();
+//7
+// function number(...numbers){
+//   console.log(numbers);
+// }
+
+// number(1, 2, 3, 4, 5)
+//8
+// function calculateTotal(...scores){
+//   let total = 0;
+//   for(let i = 0; i < scores.length; i++){  
+//     total += scores[i];
+//   }
+//   return total;
+// }
+//9
+let age = 19
+function checkAge(age){
+  if(age < 18){
+    console.log("Too Young");
+  } else {
+    console.log("Access Granted");
+  }
+}
+console.log(checkAge(17));
+
+//10
+// function f(){ return; }
